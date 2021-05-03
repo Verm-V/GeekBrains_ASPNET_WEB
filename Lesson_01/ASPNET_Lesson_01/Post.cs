5917d9136ae2e96f175af5bc7c5d070ffde93595
Verm-V <Verm-V@yandex.ru>
2021-05-03 21:39:02 +0600

    Добавлено решение задания 1-го урока

---
+++ Lesson_01/ASPNET_Lesson_01/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASPNET_Lesson_01
+{
+	/// <summary>Содержит информацию о посте</summary>
+	class Post
+	{
+		/// <summary>Id пользователя</summary>
+		public int UserId { get; set; }
+		/// <summary>Id поста</summary>
+		public int Id { get; set; }
+		/// <summary>Заголовок поста</summary>
+		public string Title { get; set; }
+		/// <summary>Содержание поста</summary>
+		public string Body { get; set; }
+
+		public override string ToString()
+		{
+			return $"{UserId}\n{Id}\n{Title}\n{Body}\n";
+		}
+	}
+}
