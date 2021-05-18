296ad19b7aa056d009e8d4f943605d6942d2cf74
Verm-V <Verm-V@yandex.ru>
2021-05-18 23:54:29 +0600

    Создан скелет контроллера для работы с классом Person и DTO запросы для него

---
+++ Lesson_03/Persons/Controllers/PersonController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeSheets.Models.Dto.Requests;
+
+namespace TimeSheets.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PersonsController : ControllerBase
+	{
+		private readonly ILogger<PersonsController> _logger;
+
+		public PersonsController(ILogger<PersonsController> logger)
+		{
+			_logger = logger;
+		}
+
+		/// <summary>
+		/// Получение информации о человеке по идентификатору
+		/// </summary>
+		/// <param name="id">Идентификатор человека</param>
+		/// <returns>Информация о запрашиваемом человеке</returns>
+		[HttpGet("{id}")]
+		public IActionResult GetPersonsById([FromRoute] int id)
+		{
+			_logger.LogDebug("Params: " +
+				$"{nameof(id)} = {id}");
+
+			return Ok();
+		}
+
+		/// <summary>
+		/// Поиск информации о человеке по его имени
+		/// </summary>
+		/// <param name="searchTerm">Имя искомого человека</param>
+		/// <returns>Информация о запрашиваемом человеке</returns>
+		[HttpGet("search")]
+		public IActionResult SearchPersonByName([FromQuery] string searchTerm)
+		{
+			_logger.LogDebug("Params: " +
+				$"{nameof(searchTerm)} = {searchTerm}");
+
+			return Ok();
+		}
+
+
+		/// <summary>
+		/// Получение списка людей с пейджингом
+		/// </summary>
+		/// <param name="skip">Номер человека с которого начинать выборку</param>
+		/// <param name="take">Количество людей в выборке</param>
+		/// <returns>Информация о запрашиваемом человеке</returns>
+		[HttpGet]
+		public IActionResult GetPersonsList([FromQuery] int? skip, [FromQuery] int? take)
+		{
+			_logger.LogDebug("Params: " +
+				$"{nameof(skip)} = {(skip.HasValue ? skip.Value.ToString() : "n/a")} " +
+				$"{nameof(take)} = {(take.HasValue ? take.Value.ToString() : "n/a")} ");
+
+			return Ok();
+		}
+
+		/// <summary>
+		/// Добавление нового человека в базу данных
+		/// </summary>
+		/// <param name="request">DTO запроса на создание человека в базе</param>
+		/// <returns>Id созданного человека</returns>
+		[HttpPost]
+		public IActionResult CreatePerson([FromBody] PersonCreateRequest request)
+		{
+			_logger.LogDebug($"Params: {Environment.NewLine}" +
+				$"{request.ToString()}");
+
+			return Ok();
+		}
+
+		/// <summary>
+		/// Изменение параметров человека в базе
+		/// </summary>
+		/// <param name="request">DTO запроса на изменение данных человека в базе</param>
+		/// <returns>Id измененного человека</returns>
+		[HttpPut]
+		public IActionResult UpdatePerson([FromBody] PersonUpdateRequest request)
+		{
+			_logger.LogDebug($"Params: {Environment.NewLine}" +
+				$"{request.ToString()}");
+
+			return Ok();
+		}
+
+		/// <summary>
+		/// Удаление человека из базы данных
+		/// </summary>
+		/// <param name="id">Идентификатор человека</param>
+		/// <returns></returns>
+		[HttpDelete("{id}")]
+		public IActionResult DeletePerson([FromRoute] int id)
+		{
+			_logger.LogDebug("Params: " +
+				$"{nameof(id)} = {id}");
+
+			return Ok();
+		}
+
+
+
+
+	}
+}
---
+++ Lesson_03/Persons/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeSheets.Models
+{
+	/// <summary>Класс описывающий человека</summary>
+	public class Person
+	{
+		/// <summary>Id</summary>
+		public int Id { get; set; }
+		/// <summary>Имя</summary>
+		public string FirstName { get; set; }
+		/// <summary>Фамилия</summary>
+		public string LastName { get; set; }
+		/// <summary>Электронная почта</summary>
+		public string Email { get; set; }
+		/// <summary>Компания</summary>
+		public string Company  { get; set; }
+		/// <summary>Возраст</summary>
+		public int Age { get; set; }
+	}
+}
